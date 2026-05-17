061210344c948152e6ac604368ba9f2dd19b8c64
kevinlqueck <kevin_luca.queckboerner@fs-students.de>
2026-05-17 11:33:38 +0200

    Update AuthenticationException.java

--- src/main/java/com/fs/srs/exceptions/AuthenticationException.java
+++ src/main/java/com/fs/srs/exceptions/AuthenticationException.java
@@ -1,6 +1,6 @@
 package com.fs.srs.exceptions;
 
-/** Thrown when a login attempt fails (bad username or password). */
+/** Thrown when a login fails beacause of wrong login data */
 public class AuthenticationException extends RuntimeException {
     public AuthenticationException(String message) {
         super(message);
